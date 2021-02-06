2b5f6a8e47db85831729cafa0a7d592dfc210d3d
Saveliy <seva007@hotmail.com>
2021-02-06 12:19:31 +0300

    Correct button input
    add range

--- static/main.js
+++ static/main.js
@@ -1,7 +1,13 @@
-const input = document.querySelector(".form input");
-const link = document.querySelector("#id-placed");
+const input     = document.querySelector(".form input");
+const link      = document.querySelector("#id-placed");
+const button    = document.querySelector("#id-b");
 
 input.addEventListener( "change", (e) => {
-    console.log( input.value )
-    link.pathname = `/mino-api/v.1.0/get-collections/${input.value}`
+    let num = parseInt(input.value)
+    if( num > 3 ) {
+        button.style.cssText = "transition: 1s;background: linear-gradient(100.28deg, #6f0000 14.35%, #440000 85.65%);";
+    } else {
+        button.style.cssText = "linear-gradient(100.28deg, #0F6F00 14.35%, #094400 85.65%)";
+        link.pathname = `/mino-api/v.1.0/get-collections/${input.value}`
+    }
 } )
