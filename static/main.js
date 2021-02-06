5fb041d33632c3df5e21ed3ca40efc52828bffea
Saveliy <seva007@hotmail.com>
2021-02-06 11:36:25 +0300

    Add href button

--- static/main.js
+++ static/main.js
@@ -1,1 +1,7 @@
-console.log( "Hello world!" )
+const input = document.querySelector(".form input");
+const link = document.querySelector("#id-placed");
+
+input.addEventListener( "change", (e) => {
+    console.log( input.value )
+    link.pathname = `/mino-api/v.1.0/get-collections/${input.value}`
+} )
